00f5c3ff42d49c912f35b036311404b8f103fd57
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2025-10-17 22:38:02 -0400

    Add root biome config

--- primal-sheets-api/drizzle-prod.config.ts
+++ primal-sheets-api/drizzle-prod.config.ts
@@ -2,11 +2,11 @@ import "dotenv/config";
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-	out: "./drizzle",
-	schema: "./src/db/schema.ts",
-	dialect: "postgresql",
-	dbCredentials: {
-		url: process.env.PROD_DATABASE_URL as string,
-	},
-	tablesFilter: ["primal-sheets_*"],
+  out: "./drizzle",
+  schema: "./src/db/schema.ts",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: process.env.PROD_DATABASE_URL as string,
+  },
+  tablesFilter: ["primal-sheets_*"],
 });
