7148ba05ca01ba87efdb2fec0c316ec9e7beed3e
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2025-11-02 00:38:17 -0400

    Update table prefix

--- src/db/table-creator.ts
+++ src/db/table-creator.ts
@@ -1,3 +1,3 @@
 import { pgTableCreator } from "drizzle-orm/pg-core";
 
-export const pgTable = pgTableCreator((name) => `primal-sheets_${name}`);
+export const pgTable = pgTableCreator((name) => `ps_${name}`);
