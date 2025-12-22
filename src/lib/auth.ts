07aa5f2089dcc1a7d72d4e4e9c347502f547444f
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2025-12-22 00:51:03 -0500

    Fix auth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -5,7 +5,6 @@ import { db } from "@/db";
 import { env } from "@/env";
 
 export const auth = betterAuth({
-	...(env.BETTER_AUTH_URL && { baseURL: env.BETTER_AUTH_URL }),
 	socialProviders: {
 		discord: {
 			clientId: env.DISCORD_CLIENT_ID,
