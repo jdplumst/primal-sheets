7d8e1139311ee1d4f8325af17593a62e59b58447
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2026-02-21 10:55:38 -0500

    Add POST campaigns route to create campaign

--- apps/api/src/features/campaigns/routes/campaign-routes.ts
+++ apps/api/src/features/campaigns/routes/campaign-routes.ts
@@ -1,12 +1,32 @@
+import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
+import z from "zod";
 import { db } from "@/db";
-import { fetchCampaignsRepository } from "@/features/campaigns/repositories/campaign-repository";
+import {
+	createCampaignRepository,
+	fetchCampaignsRepository,
+} from "@/features/campaigns/repositories/campaign-repository";
 import type { AuthenticatedContext } from "@/types";
 
-const app = new Hono<AuthenticatedContext>().get("/", async (c) => {
-	const userId = c.get("userId");
-	const campaigns = await fetchCampaignsRepository(db, userId);
-	return c.json(campaigns);
-});
+const app = new Hono<AuthenticatedContext>()
+	.get("/", async (c) => {
+		const userId = c.get("userId");
+		const campaigns = await fetchCampaignsRepository(db, userId);
+		return c.json(campaigns);
+	})
+	.post(
+		"/",
+		zValidator("json", z.object({ campaignName: z.string().min(1).max(64) })),
+		async (c) => {
+			const userId = c.get("userId");
+			const body = c.req.valid("json");
+			const createdCampaign = await createCampaignRepository(
+				db,
+				userId,
+				body.campaignName,
+			);
+			return c.json(createdCampaign);
+		},
+	);
 
 export default app;
