529440948247f881c11541bf412c575394ad67c9
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2026-03-22 14:44:48 -0400

    Strip oauth params

--- apps/web-tanstack/src/routes/_authenticated/campaigns.index.tsx
+++ apps/web-tanstack/src/routes/_authenticated/campaigns.index.tsx
@@ -1,6 +1,22 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, redirect } from "@tanstack/react-router";
+import z from "zod";
 import { Campaigns } from "@/features/campaigns/pages/campaigns";
 
+const searchSchema = z.object({
+	code: z.string().optional(),
+	state: z.string().optional(),
+});
+
 export const Route = createFileRoute("/_authenticated/campaigns/")({
+	validateSearch: searchSchema,
 	component: Campaigns,
+	beforeLoad: ({ search }) => {
+		if (search.code || search.state) {
+			throw redirect({
+				to: "/campaigns",
+				search: {},
+				replace: true,
+			});
+		}
+	},
 });
